34fb6fa243205eabdd07e67c3b8cdb64822a05e3
bbxx123 <2787922490@qq.com>
2022-09-09 15:21:32 +0800

    change：修改主页字体大小

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -108,13 +108,13 @@ export default {
     }
     .eventsWarp {
       .info {
-        font-size: 45px;
+        font-size: 35px;
         text-align: center;
         font-weight: lighter;
         color: #000;
       }
       .info2 {
-        font-size: 35px;
+        font-size: 30px;
         margin-bottom: 60px;
         text-align: center;
         font-weight: lighter;
